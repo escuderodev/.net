ed98f87dda491c9b21d74ade171b0b1835fd3c47
escuderodev <escudero.ti@gmail.com>
2024-08-02 17:36:32 -0300

    feat: att

--- ScreenSound/Program.cs
+++ ScreenSound/Program.cs
@@ -32,19 +32,16 @@ ExibirMenu();
 Console.Write("Digite a opção desejada: ");
 int opcaoEscolhida = int.Parse(Console.ReadLine()!);
 
-if (opcaoEscolhida == 1) 
+switch (opcaoEscolhida)
 {
-    Console.WriteLine($"Você escolheu a opção {opcaoEscolhida}");
-} else if (opcaoEscolhida == 2) 
-{
-    Console.WriteLine($"Você escolheu a opção {opcaoEscolhida}");
-} else if (opcaoEscolhida == 3) 
-{
-    Console.WriteLine($"Você escolheu a opção {opcaoEscolhida}");
-} else if (opcaoEscolhida == 4) 
-{
-    Console.WriteLine($"Você escolheu a opção {opcaoEscolhida}");
-} else
-{
-    Console.WriteLine("Você escolheu sair do programa!");
+    case 1: Console.WriteLine($"Você escolheu a opção {opcaoEscolhida}");
+        break;
+    case 2: Console.WriteLine($"Você escolheu a opção {opcaoEscolhida}");
+        break;
+    case 3: Console.WriteLine($"Você escolheu a opção {opcaoEscolhida}");
+        break;
+    case 4: Console.WriteLine($"Você escolheu a opção {opcaoEscolhida}");
+        break;
+    default: Console.WriteLine("Você escolheu sair do programa!");
+        break;
 }
