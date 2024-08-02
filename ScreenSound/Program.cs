64a4e491b8052b15595356c337cc70b153bf30e8
escuderodev <escudero.ti@gmail.com>
2024-08-02 18:46:31 -0300

    feat: ValidarOpcaoEscolhida method created

--- ScreenSound/Program.cs
+++ ScreenSound/Program.cs
@@ -1,6 +1,8 @@
 ﻿// ScreenSound
 string mensagem = "=== Seja bem vindo ao Screen Sound! ===";
 
+List<string> bandas = [];
+
 // funções
 void PularLinha()
 {
@@ -12,36 +14,54 @@ void ExibirMensagemDeBoasVindas()
     Console.WriteLine("=======================================");
     Console.WriteLine(mensagem);
     Console.WriteLine("=======================================");
-    PularLinha();
+    Thread.Sleep(2000);
 }
 
 void ExibirMenu()
 {
+    Console.Clear();
     Console.WriteLine("Digite 1 para registrar uma Banda.");
     Console.WriteLine("Digite 2 para listar as Bandas cadastradas.");
     Console.WriteLine("Digite 3 para avaliar uma Banda");
     Console.WriteLine("Digite 4 para exibir a média de uma Banda");
     Console.WriteLine("Digite 5 para sair do programa.");
-    PularLinha();
-};
+    ValidarOpcaoEscolhida();
+}
+
+void ValidarOpcaoEscolhida()
+{
+    Console.Write("\nDigite a opção desejada: ");
+    int opcaoEscolhida = int.Parse(Console.ReadLine()!);
+
+    switch (opcaoEscolhida)
+    {
+        case 1: RegistrarBanda();
+            break;
+        case 2: Console.WriteLine($"Você escolheu a opção {opcaoEscolhida}");
+            break;
+        case 3: Console.WriteLine($"Você escolheu a opção {opcaoEscolhida}");
+            break;
+        case 4: Console.WriteLine($"Você escolheu a opção {opcaoEscolhida}");
+            break;
+        default: Console.WriteLine("Você escolheu sair do programa!");
+            break;
+    }
+
+}
+
+void RegistrarBanda()
+{
+    Console.Clear();
+    Console.WriteLine("=== Registrando Banda... ===");
+    Console.Write("Digite o nome da Banda: ");
+    string banda = Console.ReadLine()!;
+    bandas.Add(banda);
+    Console.WriteLine($"Banda '{banda}' registrada com sucesso!");
+    Thread.Sleep(2000);
+    ExibirMenu();
+
+}
 
 // main
 ExibirMensagemDeBoasVindas();
 ExibirMenu();
-
-Console.Write("Digite a opção desejada: ");
-int opcaoEscolhida = int.Parse(Console.ReadLine()!);
-
-switch (opcaoEscolhida)
-{
-    case 1: Console.WriteLine($"Você escolheu a opção {opcaoEscolhida}");
-        break;
-    case 2: Console.WriteLine($"Você escolheu a opção {opcaoEscolhida}");
-        break;
-    case 3: Console.WriteLine($"Você escolheu a opção {opcaoEscolhida}");
-        break;
-    case 4: Console.WriteLine($"Você escolheu a opção {opcaoEscolhida}");
-        break;
-    default: Console.WriteLine("Você escolheu sair do programa!");
-        break;
-}
